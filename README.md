d7c4f3b00fd3180e68ae4c04214888029d6bbb31
TahaAyati <TahaAyati2007@gmail.com>
2026-08-15 13:46:42 -0600

    Add: Creating and building the registration controller

---
+++ README.md
@@ -0,0 +1,9 @@
+<p align="center">
+  <img src="./ItachiAcademy.jpg" alt="Itachi Academy Banner" width="100%">
+</p>
+
+<h1 align="center">Itachi Academy</h1>
+
+<p align="center">
+  Educational REST API built with Node.js
+</p>
--- controllers/v1/authController.js
+++ controllers/v1/authController.js
@@ -1,6 +1,42 @@
 const userModel = require("./../../models/users.js")
 const registerValidator = require("../../validators/auth/register.js")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 
-exports.register = async(req,res)=>{}
+exports.register = async (req, res) => {
 
-exports.login = async(req,res)=>{}
+    const validatorResult = registerValidator(req.body)
+    if (validatorResult != true) {
+        return res.status(422).json(validatorResult)
+    }
+
+    const { name, username, email, phone, password } = req.body
+
+    const isUserExist = await userModel.findOne({ $or: [{ phone }, { email }, { username }] })
+    if (isUserExist) {
+        return res.status(409).json({ message: "The phone number, email, or username is already in use !!!" })
+    }
+
+    const userCount = await userModel.countDocuments()
+    const passwordHash = await bcrypt.hash(password, 10)
+
+    const user = await userModel.create({
+        name,
+        username,
+        phone,
+        email,
+        password: passwordHash,
+        role: userCount > 0 ? "USER" : "ADMIN"
+    })
+
+
+    const acceptToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "30 day" })
+
+    const userObject = user.toObject()
+
+    Reflect.deleteProperty(userObject, "password")
+
+    return res.status(201).json({ User: userObject, acceptToken })
+}
+
+exports.login = async (req, res) => { }
