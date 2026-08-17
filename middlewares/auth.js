acd3ff4ad54bb0fa4591378d6c65647dfd039dc6
TahaAyati <TahaAyati2007@gmail.com>
2026-08-17 17:18:24 -0600

    Add: Validate user token

---
+++ middlewares/auth.js
@@ -0,0 +1,24 @@
+const userModel = require("./../models/users.js")
+const jwt = require("jsonwebtoken")
+
+module.exports = async (req, res) => {
+    const authHeader = req.header("Authorization").split(" ")
+    if (authHeader?.length !== 2) {
+        return res.status(422).json({ message: "his route is protected and you can't have access to it !!!" })
+    }
+
+    const token = authHeader[1]
+
+    try {
+        const jwtPayload = jwt.verify(token, process.env.JWT_SECRET)
+
+        const user = await userModel.findById(jwtPayload)
+
+        const userObject = user.toObject()
+        Reflect.deleteProperty(userObject, "password")
+
+        next()
+    } catch (error) {
+        return res.json(error)
+    }
+}
