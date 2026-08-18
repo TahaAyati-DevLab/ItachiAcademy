0ef7849c9e066fe7ad8459594bbae3aba8554c53
TahaAyati <TahaAyati2007@gmail.com>
2026-08-18 16:26:00 -0600

    Add: Implement functionality to retrieve the list of site users and restrict access for regular users.

--- controllers/v1/userController.js
+++ controllers/v1/userController.js
@@ -25,3 +25,8 @@ exports.ban = async (req, res) => {
 
     return res.status(201).json({ message: `The user with phone number ${isUserExist.phone} has been successfully banned.` })
 }
+
+exports.getAll = async(req,res)=>{
+    const users = await userModel.find({},"-password -_id -__v")
+    res.status(200).json(users)
+}
--- routes/v1/user.js
+++ routes/v1/user.js
@@ -1,7 +1,10 @@
 const express = require("express")
 const userRouter = express.Router()
 const userController = require("./../../controllers/v1/userController.js")
+const authMiddleware = require("./../../middlewares/auth.js")
+const isAdmin = require("./../../middlewares/isAdmin.js")
 
-userRouter.post("/ban/:id",userController.ban)
+userRouter.post("/ban/:id",authMiddleware,isAdmin,userController.ban)
+userRouter.get("/",userController.getAll)
 
 module.exports = userRouter
