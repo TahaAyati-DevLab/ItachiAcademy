6ae759abab5c3c6ca591df231b39c657e83e4293
TahaAyati <TahaAyati2007@gmail.com>
2026-08-22 11:53:09 -0600

    Fix: Fixing the issue with the category deletion route

--- routes/v1/category.js
+++ routes/v1/category.js
@@ -4,7 +4,8 @@ const categoryController = require("./../../controllers/v1/categoryController.js
 const authMiddleware = require("./../../middlewares/auth.js")
 const isAdmin = require("./../../middlewares/isAdmin.js")
 
-categoryRouter.get("/",authMiddleware,isAdmin,categoryController.getCategory)
-categoryRouter.post("/create",authMiddleware,categoryController.create)
+categoryRouter.get("/", authMiddleware, isAdmin, categoryController.getCategory)
+categoryRouter.post("/create", authMiddleware, categoryController.create)
+categoryRouter.delete("/remove/:id", authMiddleware, isAdmin, categoryController.remove)
 
 module.exports = categoryRouter
