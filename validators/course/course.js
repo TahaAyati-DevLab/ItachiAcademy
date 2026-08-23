73ccbee9dc067fb1cbc348dbd8b1c739ee6493c6
TahaAyati <TahaAyati2007@gmail.com>
2026-08-23 16:06:56 -0600

    Add: Implement validation for course creation.

---
+++ validators/course/course.js
@@ -0,0 +1,51 @@
+const validator = require("fastest-validator")
+
+const v = new validator()
+
+const schema = {
+    title: {
+        type: "string",
+        required: true
+    },
+    discription: {
+        type:"string",
+        required: true
+    },
+    cover: {
+        type: "stirng",
+        required: true
+    },
+    support: {
+        type: "string",
+        required: true
+    },
+    href: {
+        type: "string",
+        required: true
+    },
+    price: {
+        type: "string",
+        required: true
+    },
+    status: {
+        type: "string",
+        required: true
+    },
+    discount: {
+        type: "string",
+        required: true
+    },
+    categoryID: {
+        type: "string",
+        required: true
+    },
+    teacher: {
+       type:"string",
+        required: true
+    },
+  $$strict: true
+}
+
+const check = v.compile(schema)
+
+module.exports = check
