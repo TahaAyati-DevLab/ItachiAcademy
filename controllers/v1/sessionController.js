3d1580a613ec624f8fe2bde206e26021f7b4ce0d
TahaAyati <TahaAyati2007@gmail.com>
2026-08-25 16:37:04 -0600

    Fix: Resolved several issues in sessionController.

--- controllers/v1/sessionController.js
+++ controllers/v1/sessionController.js
@@ -1,16 +1,30 @@
 const sessionModel = require("./../../models/session.js")
 const courseModel = require("./../../models/courses.js")
+const { isValidObjectId } = require("mongoose")
 
-exports.create = async(req,res)=>{
-    const {title,time,free,course} = req.body
+exports.create = async (req, res) => {
+
+    const id = req.params.id
+    if (!isValidObjectId(id)) {
+        return res.status(422).json({ message: "The entered ID is invalid." })
+    }
+
+
+    const { title, time, free, course } = req.body
+
+    const isCourseExist = await courseModel.findById({_id:id})
+
+    if(!isCourseExist){
+        return res.status(404).json({message:"The requested course was not found."})
+    }
 
     const session = await sessionModel.create({
         title,
         time,
         free,
-        video:"Test.MP4",
-        course
+        video: "Test.MP4",
+        course: id
     })
 
-    return res.status(201).json({message:"The course was successfully created."})
+    return res.status(201).json({ message: "The course was successfully created." })
 }
--- routes/v1/session.js
+++ routes/v1/session.js
@@ -2,6 +2,6 @@ const express = require("express")
 const sessionRouter = express.Router()
 const sessionController = require("./../../controllers/v1/sessionController.js")
 
-sessionRouter.post("/create",sessionController.create)
+sessionRouter.post("/:id/create",sessionController.create)
 
 module.exports = sessionRouter
