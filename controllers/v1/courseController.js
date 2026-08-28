f6b90d329410e1aea68e0ee89081d9d08a0231a6
TahaAyati <TahaAyati2007@gmail.com>
2026-08-28 23:06:55 +0330

    Add: Implementing the ability to retrieve a list of courses for a specific category.

--- controllers/v1/courseController.js
+++ controllers/v1/courseController.js
@@ -2,6 +2,7 @@ const courseModel = require("./../../models/courses.js")
 const sessionModel = require("./../../models/session.js")
 const courseUser = require("./../../models/course-user.js")
 const courseValidator = require("./../../validators/course/course.js")
+const categoryModel = require("./../../models/category.js")
 
 exports.createCourse = async (req, res) => {
     const validationResult = courseValidator(req.body)
@@ -40,20 +41,40 @@ exports.getSessionInfo = async (req, res) => {
 
     console.log(sessions)
 
-    return res.json({session,sessions})
+    return res.json({ session, sessions })
 }
 
-exports.register = async(req,res)=>{
-    const isUserAlerdayRegistered = await courseUser.findOne({user:req.user._id})
-    if(isUserAlerdayRegistered){
-        return res.status(409).json({message:"You have already registered for the course."})
-    } 
+exports.register = async (req, res) => {
+    const isUserAlerdayRegistered = await courseUser.findOne({ user: req.user._id })
+    if (isUserAlerdayRegistered) {
+        return res.status(409).json({ message: "You have already registered for the course." })
+    }
 
     const register = await courseUser.create({
-        user:req.user._id,
-        course:req.params.id,
-        price:req.body.price
+        user: req.user._id,
+        course: req.params.id,
+        price: req.body.price
     })
 
-    return res.status(201).json({message:"You have successfully registered for the course."})
+    return res.status(201).json({ message: "You have successfully registered for the course." })
 }
+
+exports.allCourseThisCategory = async (req, res) => {
+    const { href } = req.params;
+
+    const category = await categoryModel.findOne({ href });
+
+    if (!category) {
+        return res.status(404).json({
+            message: "Category not found..."
+        });
+    }
+
+    const courses = await courseModel.find({
+        categoryID: category._id
+    });
+
+    return res.status(200).json({
+        courses
+    });
+};
