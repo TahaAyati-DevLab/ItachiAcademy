f0ed41226697b8013fe252a031608a3b3f9a6b82
TahaAyati <TahaAyati2007@gmail.com>
2026-08-29 22:24:55 +0330

    Add: Development of the ability to reject or approve submitted comments.

--- controllers/v1/commentController.js
+++ controllers/v1/commentController.js
@@ -1,34 +1,55 @@
 const commentModel = require("./../../models/comments")
-const {isValidObjectId} = require("mongoose")
+const { isValidObjectId } = require("mongoose")
 
-exports.create = async(req,res)=>{
-    const {body, creator,courseHref,score} = req.body
+exports.create = async (req, res) => {
+    const { body, creator, courseHref, score } = req.body
 
     const comment = await commentModel.create({
         body,
-        user : req.user._id,
-        course : courseHref,
+        user: req.user._id,
+        course: courseHref,
         score,
-        mainCommentID : 0,
-        isAccept : 0,
-        isAnswer:0
+        mainCommentID: 0,
+        isAccept: 0,
+        isAnswer: 0
     })
 
-    return res.status(201).json({message:"Create Comment Successfully..."})
+    return res.status(201).json({ message: "Create Comment Successfully..." })
 }
 
-exports.remove = async(req,res)=>{
-    const {id} = req.params;
+exports.remove = async (req, res) => {
+    const { id } = req.params;
 
-    if(!isValidObjectId(id)){
-        return res.status(422).json({message:"The entered ID is invalid."})
+    if (!isValidObjectId(id)) {
+        return res.status(422).json({ message: "The entered ID is invalid." })
     }
 
-    const comment = await commentModel.findOneAndDelete({_id:id})
+    const comment = await commentModel.findOneAndDelete({ _id: id })
 
-    if(!comment){
-        return res.status(404).json({message:"No comment with this ID was found."})
+    if (!comment) {
+        return res.status(404).json({ message: "No comment with this ID was found." })
     }
 
-    return res.status(201).json({message:"Comment successfully deleted."})
+    return res.status(201).json({ message: "Comment successfully deleted." })
+}
+
+exports.accept = async (req, res) => {
+    const acceptComment = await commentModel.findByIdAndUpdate({ _id: req.params.id }, {
+        isAccept: 1
+    })
+    if (!acceptComment) {
+        return res.status(404).json({ message: "No comment with this ID was found." })
+    }
+        return res.status(201).json({ message: "Comment successfully accept." })
+
+}
+exports.reject = async (req, res) => {
+    const rejectComment = await commentModel.findByIdAndUpdate({ _id: req.params.id }, {
+        isAccept: 0
+    })
+
+        if (!rejectComment) {
+        return res.status(404).json({ message: "No comment with this ID was found." })
+    }
+        return res.status(201).json({ message: "Comment successfully reject." })
 }
--- routes/v1/comments.js
+++ routes/v1/comments.js
@@ -6,5 +6,8 @@ const isAdmin = require("./../../middlewares/isAdmin.js")
 
 commentRouter.post("/", authMiddleware, commentController.create)
 commentRouter.delete("/remove/:id", authMiddleware, isAdmin, commentController.remove)
+commentRouter.put("/:id/accept",commentController.accept)
+commentRouter.put("/:id/reject",commentController.reject)
+
 
 module.exports = commentRouter
