319fc19156d9239f4b941982b49cc31c9c88247b
TahaAyati <TahaAyati2007@gmail.com>
2026-09-04 13:04:55 +0330

    Add: Receive different units for sending tickets.

--- controllers/v1/ticketController.js
+++ controllers/v1/ticketController.js
@@ -1,7 +1,27 @@
+const departmenModel = require("./../../models/departmen.js")
+const departmenSubModel = require("./../../models/departmen-sub.js")
+
 exports.create = async (req, res) => { }
+
 exports.getAll = async (req, res) => { }
+
 exports.userTickets = async (req, res) => { }
-exports.departments = async (req, res) => { }
-exports.departmentsSubs = async (req, res) => { }
+
+exports.departments = async (req, res) => {
+    const departmen = await departmenModel.find({}).lean()
+    return res.status(200).json(departmen)
+}
+
+exports.departmentsSubs = async (req, res) => {
+    const { id } = req.params
+    const departmen = await departmenModel.findById({ _id: id })
+    const departmenSub = await departmenSubModel.find({ parent: departmen._id }).populate("parent", "title")
+
+    return res.status(200).json(departmenSub)
+
+
+}
+
 exports.setAnswer = async (req, res) => { }
+
 exports.getAnswer = async (req, res) => { }
